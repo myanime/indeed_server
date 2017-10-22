5f4eb3cbc9cbcb7f8b23f5c65751ad71158904f0
myanime <ryan.cassels@hotmail.com>
2017-10-22 14:29:39 +0200

    Error Handling

--- indeed/spiders/SeleniumSpider.py
+++ indeed/spiders/SeleniumSpider.py
@@ -6,7 +6,7 @@ import re
 import traceback
 import hashlib
 from selenium import webdriver
-
+from selenium.common.exceptions import NoSuchElementException
 loaded_counter = int([line.rstrip('\n') for line in open('./static/counter')][0])
 
 main_counter = loaded_counter + 1000
@@ -272,84 +272,85 @@ class MainScraper(scrapy.Spider):
         driver = webdriver.Chrome('./chromedriver')
         start_urls = [line.rstrip("\n") for line in open('./static/indeedurls')]
         for url in start_urls:
-            driver.get(url)
-            job_add = driver.find_elements_by_xpath('//h2')
+            try:
+                driver.get(url)
+                job_add = driver.find_elements_by_xpath('//h2')
 
-            for add in job_add:
-                def get_money(money):
-                    job_money = None
-                    range_lower = None
-                    range_upper = None
-                    salary_description = None
-                    job_money_unchanged = None
-                    if money:
-                        try:
-                            job_money = money[0].text
-                            job_money_unchanged = job_money
-                            job_money = job_money.replace(',', '')
-                            job_money = job_money.replace('$', '')
-                            if re.search(r' a year', job_money):
-                                job_money = job_money.split(' a year')[0]
-                                salary_description = 'a year'
-                            if re.search(r' an hour', job_money):
-                                job_money = job_money.split(' an hour')[0]
-                                salary_description = 'an hour'
-                            if re.search(r' a week', job_money):
-                                job_money = job_money.split(' a week')[0]
-                                salary_description = 'a week'
-                            if re.search(r' a day', job_money):
-                                job_money = job_money.split(' a day')[0]
-                                salary_description = 'a day'
-                            if re.search(r'-', job_money):
-                                range_lower = job_money.split(" - ")[0]
-                                range_upper = job_money.split(" - ")[1]
-                        except:
-                            job_money = None
-                    return job_money, range_lower, range_upper, salary_description, job_money_unchanged
+                for add in job_add:
+                    def get_money(money):
+                        job_money = None
+                        range_lower = None
+                        range_upper = None
+                        salary_description = None
+                        job_money_unchanged = None
+                        if money:
+                            try:
+                                job_money = money[0].text
+                                job_money_unchanged = job_money
+                                job_money = job_money.replace(',', '')
+                                job_money = job_money.replace('$', '')
+                                if re.search(r' a year', job_money):
+                                    job_money = job_money.split(' a year')[0]
+                                    salary_description = 'a year'
+                                if re.search(r' an hour', job_money):
+                                    job_money = job_money.split(' an hour')[0]
+                                    salary_description = 'an hour'
+                                if re.search(r' a week', job_money):
+                                    job_money = job_money.split(' a week')[0]
+                                    salary_description = 'a week'
+                                if re.search(r' a day', job_money):
+                                    job_money = job_money.split(' a day')[0]
+                                    salary_description = 'a day'
+                                if re.search(r'-', job_money):
+                                    range_lower = job_money.split(" - ")[0]
+                                    range_upper = job_money.split(" - ")[1]
+                            except:
+                                job_money = None
+                        return job_money, range_lower, range_upper, salary_description, job_money_unchanged
 
-                add = add.find_element_by_xpath('..')
+                    add = add.find_element_by_xpath('..')
 
-                job_title = add.find_element_by_xpath('h2').text
-                job_description = add.find_element_by_css_selector('span.summary').text.replace('\n', '')
-                job_location = add.find_element_by_css_selector('span.location').text
-                job_date = add.find_element_by_css_selector('span.location').text
-                job_company = add.find_element_by_css_selector('span.company').text
-                money = add.find_elements_by_css_selector('span.no-wrap')
-                job_money, range_lower, range_upper, salary_description, job_money_unchanged = get_money(money)
+                    job_title = add.find_element_by_xpath('h2').text
+                    job_description = add.find_element_by_css_selector('span.summary').text.replace('\n', '')
+                    job_location = add.find_element_by_css_selector('span.location').text
+                    job_date = add.find_element_by_css_selector('span.location').text
+                    job_company = add.find_element_by_css_selector('span.company').text
+                    money = add.find_elements_by_css_selector('span.no-wrap')
+                    job_money, range_lower, range_upper, salary_description, job_money_unchanged = get_money(money)
 
-                image_link = None
-                full_link = add.find_element_by_xpath('h2/a').get_attribute('href')
+                    image_link = None
+                    full_link = add.find_element_by_xpath('h2/a').get_attribute('href')
 
-                print full_link
-                print job_title
-                print job_description
-                print job_location
-                print job_company
-                print job_date
-                print job_money
-                print "-------------"
-
-
-
-                item = IndeedItem()
-                global main_counter
-                main_counter = main_counter + 1
-                with open('./static/counter', 'w') as f:
-                    f.write(str(main_counter))
-                item['jobNumber'] = None
-                item['job_title'] = job_title
-                item['job_description'] = job_description
-                item['job_location'] = job_location
-                item['job_company'] = job_company
-                days_date = time.strftime("%d_%m_%Y")
-                item['job_date'] = days_date
-                item['indeed_date'] = job_date
-                item['job_money'] = job_money
-                item['range_upper'] = range_upper
-                item['job_money_unchanged'] = job_money_unchanged
-                item['range_lower'] = range_lower
-                item['salary_description'] = salary_description
-                item['image_link'] = image_link
-                request = scrapy.Request(full_link, callback=self.parse_original_url)
-                request.meta['item'] = item
-                yield request
+                    item = IndeedItem()
+                    global main_counter
+                    main_counter = main_counter + 1
+                    with open('./static/counter', 'w') as f:
+                        f.write(str(main_counter))
+                    item['jobNumber'] = None
+                    item['job_title'] = job_title
+                    item['job_description'] = job_description
+                    item['job_location'] = job_location
+                    item['job_company'] = job_company
+                    days_date = time.strftime("%d_%m_%Y")
+                    item['job_date'] = days_date
+                    item['indeed_date'] = job_date
+                    item['job_money'] = job_money
+                    item['range_upper'] = range_upper
+                    item['job_money_unchanged'] = job_money_unchanged
+                    item['range_lower'] = range_lower
+                    item['salary_description'] = salary_description
+                    item['image_link'] = image_link
+                    request = scrapy.Request(full_link, callback=self.parse_original_url)
+                    request.meta['item'] = item
+                    yield request
+            except NoSuchElementException:
+                print "#########################################"
+                print "#########################################"
+                print "##############SELENIUM ERROR#############"
+                print "#########################################"
+                print "#########################################"
+                problem = traceback.format_exc()
+                with open('./static/errors.txt', 'a') as f:
+                    f.write(problem)
+                    f.write("################################")
+                time.sleep(2)
