d8a5776d8dd2e9242925c2d14330cb3a870caf4d
Ryan <ryan.cassels@hotmail.com>
2019-06-14 19:50:53 +0200

    fixed cron

--- poorman_cron.sh
+++ poorman_cron.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+COUNTRY='au'
 echo "Poor Mans Cron"
 sleep 5
 echo "Starting"
-gnome-terminal --command=/home/myanime/indeed_server/local_indeed.sh --display=:0
+gnome-terminal --command=/home/myanime/indeed_server$COUNTRY/local_indeed.sh $COUNTRY $COUNTRY --display=:0
 
 while true
 do
